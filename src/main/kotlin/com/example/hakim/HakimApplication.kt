46e377d608e55cbae1188b3347d64596c8067f66
sokitbayev <sokitbayev@gmail.com>
2022-03-23 23:10:59 +0600

    a

--- src/main/kotlin/com/example/hakim/HakimApplication.kt
+++ src/main/kotlin/com/example/hakim/HakimApplication.kt
@@ -2,10 +2,23 @@ package com.example.hakim
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class HakimApplication
 
 fun main(args: Array<String>) {
 	runApplication<HakimApplication>(*args)
+}
+
+
+@RestController
+class ExampleController() {
+
+	@GetMapping("/name")
+	fun name(): String {
+		return "Tlek".repeat(100).plus("Tlek\n".repeat(100000))
+	}
+
 }
