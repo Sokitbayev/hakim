a5867390de6ad67507c0afefcd51b2b5736e5b23
sokitbayev <sokitbayev@gmail.com>
2022-03-23 23:04:00 +0600

    Initial commit

---
+++ src/main/kotlin/com/example/hakim/HakimApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hakim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HakimApplication
+
+fun main(args: Array<String>) {
+	runApplication<HakimApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/hakim/HakimApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.hakim
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HakimApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
